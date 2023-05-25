9bef060d38fa3090f70fefd2f3385205970f1c56
superheher <heh@vivaldi.net>
2023-05-25 16:46:50 +0300

    Replaced ListView for game search results with RecyclerView.

--- app/src/main/java/com/example/twitchadminpanel/SecondFragment.java
+++ app/src/main/java/com/example/twitchadminpanel/SecondFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -36,7 +37,7 @@ import java.util.Map;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
-    private ArrayAdapter<String> adapter;
+    private GameSearchAdapter adapter;
 
     @Override
     public View onCreateView(
@@ -65,12 +66,6 @@ public class SecondFragment extends Fragment {
                     .navigate(R.id.action_SecondFragment_to_FirstFragment);
         });
 
-        binding.gameSearchResult.setOnItemClickListener((parent, view12, position, id) -> {
-            String item = binding.gameSearchResult.getItemAtPosition(position).toString();
-            String[] data = item.split(MainActivity.SEPARATOR_GAME_ID);
-            binding.gameEditor.setText(data[0]);
-            binding.gameId.setText(data[1]);
-        });
         binding.search.setOnClickListener(view13 -> {
             RequestQueue queue = Volley.newRequestQueue(getContext());
             String url = "https://api.twitch.tv/helix/search/categories?query=" + binding.gameEditor.getText();
@@ -89,9 +84,15 @@ public class SecondFragment extends Fragment {
                     }
                 } catch (JSONException ignored) {
                 }
-                adapter = new ArrayAdapter<String>(getContext(), R.layout.simple_list_item_1, strings);
+                adapter = new GameSearchAdapter(getContext(), strings);
                 binding.gameSearchResult.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
+                binding.gameSearchResult.setLayoutManager(new LinearLayoutManager(getContext()));
+                adapter.setClickListener((View v, int position) -> {
+                    String item = adapter.getItem(position);
+                    String[] data = item.split(MainActivity.SEPARATOR_GAME_ID);
+                    binding.gameEditor.setText(data[0]);
+                    binding.gameId.setText(data[1]);
+                });
             }, error -> {
             }) {
                 @Override
